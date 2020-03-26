3761c18c4c51a8c2387cfce805cf5ffb810948d9
Madhavi <Madhavi@PC06292019>
2020-03-26 17:22:06 -0400

    User2 changes

--- gs-maven/src/main/java/hello/Greeter.java
+++ gs-maven/src/main/java/hello/Greeter.java
@@ -6,6 +6,10 @@ public class Greeter {
 		// Changes by User1
 		System.out.println("*******************User1");
 		System.out.println("*******************");
+		
+		// Changes by User2
+		System.out.println("*******************User2");
+		System.out.println("*******************");
 		return "Hello world!";
 	}
 }
