dd731a38c24d758b1ad80922234bc7169931fa9e
Madhavi <Madhavi@PC06292019>
2020-03-26 17:05:25 -0400

    Changes by User1

--- gs-maven/src/main/java/hello/Greeter.java
+++ gs-maven/src/main/java/hello/Greeter.java
@@ -3,6 +3,8 @@ package hello;
 public class Greeter {
 	public String sayHello() {
 		
+		// Changes by User1
+		System.out.println("*******************User1");
 		System.out.println("*******************");
 		return "Hello world!";
 	}
